fe56f903fe35ee5950154e4db3c4581c8be5f639
Ymir-badam <129545041+Ymir-badam@users.noreply.github.com>
2023-10-02 16:55:44 +0530

    Create Reverse_the_Array.py

---
+++ Python/Reverse_the_Array.py
@@ -0,0 +1,13 @@
+def reverseList1(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+def reverseList2(A):
+  A=A[::-1]
+
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
